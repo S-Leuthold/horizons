565c2477aa3d401a336d14b63f0421636c4460b8
Sam Leuthold <60447664+S-Leuthold@users.noreply.github.com>
2025-06-12 15:05:40 -0500

    Update safely_execute to optionally return error

---
+++ tests/testthat/test-safely_execute.R
@@ -0,0 +1,17 @@
+test_that("safely_execute returns value when no error", {
+  res <- horizons:::safely_execute({1 + 1})
+  expect_equal(res, 2)
+})
+
+test_that("safely_execute can return result list", {
+  res <- horizons:::safely_execute({1 + 1}, return_result_list = TRUE)
+  expect_equal(res$result, 2)
+  expect_null(res$error)
+})
+
+test_that("safely_execute returns default and error on failure", {
+  res <- horizons:::safely_execute({stop("oops")}, default_value = NA,
+                                    log_error = FALSE, return_result_list = TRUE)
+  expect_true(inherits(res$error, "error"))
+  expect_true(is.na(res$result))
+})
